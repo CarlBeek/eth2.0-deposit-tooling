ff10147978b31262545f84dd0caebb2e1a0aed17
Carl Beekhuizen <carl.beek@gmail.com>
2019-07-06 16:25:40 +0200

    Initial commit

---
+++ deposit_script/key_gen.py
@@ -0,0 +1,32 @@
+from py_ecc.optimized_bls12_381.optimized_curve import curve_order
+from secrets import randbelow
+from typing import Tuple
+from utils import (
+    int_to_int_hash,
+    num_bits_to_num_bytes,
+    hash_func_bytes,
+    KeyPair,
+)
+
+
+def generate_withdrawal_keys() -> KeyPair:
+    return KeyPair(privkey=randbelow(curve_order))
+
+
+def derive_signing_keys(withdrawal_keys: KeyPair) -> KeyPair:
+    # TODO: Consider replacing with BIP32 key derivation
+    withdrawal_key = withdrawal_keys.privkey
+    num_bytes = num_bits_to_num_bytes(curve_order.bit_length())
+    assert hash_func_bytes >= num_bytes  # Sanity check that the hash func generates sufficient entropy
+    while True:
+        key = int_to_int_hash(withdrawal_key, num_bytes)
+        if key > curve_order:
+            withdrawal_key = key
+            continue
+        return KeyPair(key)
+
+
+def generate_key_pairs() -> Tuple[KeyPair, KeyPair]:
+    withdrawal_keys = generate_withdrawal_keys()
+    signing_keys = derive_signing_keys(withdrawal_keys)
+    return withdrawal_keys, signing_keys
---
+++ deposit_script/utils.py
@@ -0,0 +1,30 @@
+from py_ecc.bls.api import privtopub
+from dataclasses import dataclass
+from hashlib import sha256
+
+ENDIANNESS = 'little'
+_hash_func = sha256
+
+
+def hash(x):
+    return _hash_func(x).digest()
+
+
+def int_to_int_hash(x: int, num_bytes: int) -> int:
+    hashed_int = hash(x.to_bytes(num_bytes, byteorder=ENDIANNESS))
+    return int.from_bytes(hashed_int, byteorder=ENDIANNESS)
+
+
+def num_bits_to_num_bytes(x: int) -> int:
+    return -(-x//8)
+
+
+hash_func_bytes = _hash_func().digest_size
+
+
+@dataclass(frozen=True)
+class KeyPair:
+    privkey: int
+
+    def __post_init__(self):
+        self.pubkey = privtopub(self.privkey)
