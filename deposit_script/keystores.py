8905e4c8a2e3c392e200f720f60ded63631c3cbb
Carl Beekhuizen <carl.beek@gmail.com>
2019-07-16 12:27:23 +0200

    JSON-ify KeyStores

--- deposit_script/keystores.py
+++ deposit_script/keystores.py
@@ -1,6 +1,7 @@
 from dataclasses import (
     dataclass,
     asdict,
+    fields,
 )
 import json
 from secrets import randbits
@@ -16,9 +17,30 @@ from utils.crypto import (
     AES,
 )
 
+hexdigits = set('0123456789abcdef')
+
+
+def to_bytes(obj):
+    if isinstance(obj, str):
+        if all(c in hexdigits for c in obj):
+            return bytes.fromhex(obj)
+    elif isinstance(obj, dict):
+        for key, value in obj.items():
+            if key == 'iv':
+                continue
+            obj[key] = to_bytes(value)
+    return obj
+
+
+class BytesDataclass:
+    def __post_init__(self):
+        for field in fields(self):
+            if field.type in (dict, bytes):
+                self.__setattr__(field.name, to_bytes(self.__getattribute__(field.name)))
+
 
 @dataclass
-class KeyStoreCrypto:
+class KeyStoreCrypto(BytesDataclass):
     cipher: str
     cipherparams: dict
     ciphertext: bytes
@@ -28,10 +50,21 @@ class KeyStoreCrypto:
 
 
 @dataclass
-class KeyStore:
+class KeyStore(BytesDataclass):
     crypto: KeyStoreCrypto
     id: str
     version: int
+
+    def as_json(self) -> str:
+        return json.dumps(asdict(self), default=lambda x: x.hex())
+
+    @classmethod
+    def from_json(cls, json_str: str):
+        json_dict = json.loads(json_str)
+        crypto = KeyStoreCrypto(**json_dict['crypto'])
+        id = json_dict['id']
+        version = json_dict['version']
+        return cls(crypto=crypto, id=id, version=version)
 
 
 class ScryptKeyStore(KeyStore):
@@ -53,11 +86,8 @@ class ScryptKeyStore(KeyStore):
 
     def __init__(self, *, secret: bytes, password: KeystorePassword,
                  salt: Optional[KeystoreSalt]=None, iv: Optional[AESIV]=None):
-        self.crypto.kdfparams['salt'] = salt if salt is not None else KeystoreSalt(hex(randbits(256))[2:])
-        self.crypto.cipherparams['iv'] = iv if iv is not None else AESIV(hex(randbits(128))[2:])
+        self.crypto.kdfparams['salt'] = salt if KeystoreSalt(hex(randbits(256))[2:]) is None else to_bytes(salt)
+        self.crypto.cipherparams['iv'] = iv if AESIV(hex(randbits(128))[2:]) is None else iv
         decryption_key = scrypt(password=password, **self.crypto.kdfparams)
         self.crypto.ciphertext = AES(key=decryption_key[:16], secret=secret, iv=self.crypto.cipherparams['iv'])
         self.crypto.mac = keccak(decryption_key[16:32] + self.crypto.ciphertext)
-
-    def as_json(self) -> str:
-        return json.dumps(asdict(self), default=lambda x: x.hex())
--- deposit_script/tests/test_keystores.py
+++ deposit_script/tests/test_keystores.py
@@ -1,35 +1,45 @@
 from keystores import (
     KeyStore,
-    KeyStoreCrypto,
     ScryptKeyStore,
 )
+
+from json import loads
 
 #  Test vector from Eth Wiki: https://github.com/ethereum/wiki/wiki/Web3-Secret-Storage-Definition#scrypt
 test_password = 'testpassword'
 test_secret = bytes.fromhex("7a28b5ba57c53603b0b07b56bba752f7784bf506fa95edc395f5cf6c7514fe9d")
-crypt = KeyStoreCrypto(
-    cipher="aes-128-ctr",
-    cipherparams={"iv": "83dbcc02d8ccb40e466191a123791e0e"},
-    ciphertext=bytes.fromhex("d172bf743a674da9cdad04534d56926ef8358534d458fffccd4e6ad2fbde479c"),
-    kdf="scrypt",
-    kdfparams={
-        "dklen": 32,
-        "n": 262144,
-        "r": 1,
-        "p": 8,
-        "salt": bytes.fromhex("ab0c7876052600dd703518d6fc3fe8984592145b591fc8fb5c6d43190334ba19"),
+test_keystore_json = '''
+{
+    "crypto" : {
+        "cipher" : "aes-128-ctr",
+        "cipherparams" : {
+            "iv" : "83dbcc02d8ccb40e466191a123791e0e"
+        },
+        "ciphertext" : "d172bf743a674da9cdad04534d56926ef8358534d458fffccd4e6ad2fbde479c",
+        "kdf" : "scrypt",
+        "kdfparams" : {
+            "dklen" : 32,
+            "n" : 262144,
+            "r" : 1,
+            "p" : 8,
+            "salt" : "ab0c7876052600dd703518d6fc3fe8984592145b591fc8fb5c6d43190334ba19"
+        },
+        "mac" : "2103ac29920d71da29f15d75b4a16dbe95cfd7ff8faea1056c33131d846e3097"
     },
-    mac=bytes.fromhex("2103ac29920d71da29f15d75b4a16dbe95cfd7ff8faea1056c33131d846e3097")
-)
-test_scrypt_keystore = KeyStore(
-    crypto=crypt,
-    id="3198bc9c-6672-5ab3-d995-4942343ae5b6",
-    version=3,
-)
+    "id" : "3198bc9c-6672-5ab3-d995-4942343ae5b6",
+    "version" : 3
+}'''
+
+
+def test_json_serialization():
+    keystore = KeyStore.from_json(test_keystore_json)
+    assert loads(keystore.as_json()) == loads(test_keystore_json)
 
 
 def test_mac():
-    iv = test_scrypt_keystore.crypto.cipherparams['iv']
-    salt = test_scrypt_keystore.crypto.kdfparams['salt']
-    keystore = ScryptKeyStore(test_secret, test_password, salt=salt, iv=iv)
-    assert keystore.crypto == test_scrypt_keystore.crypto
+    iv = loads(test_keystore_json)['crypto']['cipherparams']['iv']
+    salt = loads(test_keystore_json)['crypto']['kdfparams']['salt']
+    keystore = ScryptKeyStore(secret=test_secret, password=test_password, salt=salt, iv=iv)
+    print(keystore.crypto)
+    print(KeyStore.from_json(test_keystore_json).crypto)
+    assert keystore.crypto == KeyStore.from_json(test_keystore_json).crypto
