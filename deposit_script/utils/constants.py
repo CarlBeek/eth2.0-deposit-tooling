8d1699823907f1999da196de96b1dead4be77771
Carl Beekhuizen <carl.beek@gmail.com>
2019-07-22 12:15:09 +0200

    BIP44 constants

--- deposit_script/utils/constants.py
+++ deposit_script/utils/constants.py
@@ -2,4 +2,5 @@ ENDIANNESS = 'little'
 DOMAIN_DEPOSIT = int(3).to_bytes(4, byteorder=ENDIANNESS)
 ZERO_BYTES32 = b'\x00' * 32
 
-TX_BROADCASTING_URLS = ['https://etherscan.io/pushTx?hex=%s']
+TX_BROADCASTING_URLS = ['https://etherscan.io/pushTx?hex=%s']  # URLS to which support Ethereum TX
+COIN_TYPE = 60**2  # = 3600 BIP44 coin-type (60**2 is the second iteration of the Ethereum Coin Type)
