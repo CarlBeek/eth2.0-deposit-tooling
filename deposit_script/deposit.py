13f0f628545492b2e9226680f08a214453732360
Carl Beekhuizen <carl.beek@gmail.com>
2019-07-24 14:51:14 +0200

    Adapt args to use mnemonic

--- deposit_script/deposit.py
+++ deposit_script/deposit.py
@@ -1,32 +1,35 @@
 from argparse import ArgumentParser
-# from eth2.bls_signers import (
-#     WithdrawalCredentials,
-#     SigningCredentials,
-# )
+from key_derivation.bip39 import (
+    get_mnemonic,
+    get_seed,
+)
+from key_derivation.bip32 import derive_master_privkey
 
 
 def get_args():
     parser = ArgumentParser(description='🦄 : the validator deposit assistant')
-    parser.add_argument('withdraw_pwd', type=str, help='The password to encrypt the Eth2 withdrawal keystores')
-    parser.add_argument('--signing_pwd', type=str, help='The password to encrypt your Eth2 Validator signing keystores (Default is to reuse `withdraw_pwd`)')  # noqa: E501
-    parser.add_argument('num_validators', type=int, help='The number of validator instances you want to create for Eth2 (Requires a 32 Eth deposit each)')  # noqa: E501
+    parser.add_argument('--withdraw_pwd', type=str, required=True, help='Eth2 withdrawal keystores password')
+    parser.add_argument('--signing_pwd', type=str, help='Eth2 signing keystores password (Default is to reuse `withdraw_pwd`)')  # noqa: E501
+    parser.add_argument('--seed_pwd', type=str, default='', help='Password for additional seed-phrase security. (Default is None)')  # noqa: E501
+    parser.add_argument('--num_validators', type=int, required=True, help='Number of Eth2 validator instances to create. (Each requires a 32 Eth deposit)')  # noqa: E501
     args = parser.parse_args()
     args.signing_pwd = args.withdraw_pwd if args.signing_pwd is None else args.signing_pwd
     return args
 
 
 def generate_bls_credentials(args):
-    withdrawal_credentials = []
-    signing_credentials = []
-    # for validator in range(args.num_validators):
-    #     withdrawal_credentials.append(WithdrawalCredentials())
-    #     signing_credentials.append(SigningCredentials(withdrawal_credentials[-1]))
-    return withdrawal_credentials, signing_credentials
+    mnemonic = get_mnemonic()
+    print('Below is your seed phrase. Write it down and store it in a safe place. It is the ultimate backup of all your Eth2.0 Eth and signing keys')  # noqa: E501
+    print('\n\n\n\n%s\n\n\n\n' % mnemonic)
+    input("Press Enter to continue...")
+    seed = get_seed(mnemonic=mnemonic, password=args.seed_pwd)
+    master_privkey, master_chaincode = derive_master_privkey(seed)
+    print(master_privkey)
 
 
 def main():
     args = get_args()
-    withdrawal_keystores, signing_keystores = generate_bls_credentials(args)
+    generate_bls_credentials(args)
 
 
 if __name__ == '__main__':
