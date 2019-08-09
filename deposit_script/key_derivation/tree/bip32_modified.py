ccd5282aca20f43c175774bd77203280c7ec35d6
Carl Beekhuizen <carl.beek@gmail.com>
2019-08-09 16:29:00 +0200

    Removes chain-code from tree-KDF

--- deposit_script/key_derivation/tree/bip32_modified.py
+++ deposit_script/key_derivation/tree/bip32_modified.py
@@ -1,72 +1,25 @@
-from utils.crypto import hmac_sha512
-from utils.typing import (
-    BLSPubkey,
-    BLSPrivkey,
-)
-from typing import Tuple
+from utils.crypto import sha256
 from utils.bls import (
     bls_curve_order,
-    bls_priv_to_pub,
 )
 
 
-def point(p: int) -> BLSPubkey:
-    return bls_priv_to_pub(BLSPrivkey(p))
-
-
-def ser_256(p: int) -> bytes:
-    return p.to_bytes(32, byteorder='big')
-
-
-def ser_32(i: int) -> bytes:
-    return i.to_bytes(4, byteorder='big')
-
-
-def de_ser_int(b: bytes) -> int:
-    return int.from_bytes(b, 'big')
-
-
-def ser_p(p: BLSPubkey) -> bytes:
-    x, y = p
-    flags = b'\x02' if y % 2 == 0 else b'\x03'
-    return flags + ser_256(x)
-
-
-def bytes_xor(a: bytes, b: bytes) -> bytes:
-    length = max(len(a), len(b))
-    int_a = int.from_bytes(a, 'big')
-    int_b = int.from_bytes(b, 'big')
-    int_ab = int_a ^ int_b
-    return int_ab.to_bytes(length, 'big')
-
-
-def get_valid_I(*, chain_code: bytes, data: bytes) -> bytes:
-    # Performs rejection sampling until the returned point is below the curve order
+def int_to_int_hash(x: int) -> int:
     while True:
-        I = hmac_sha512(key=chain_code, msg=data)  # noqa: E741
-        I_left = I[:32]
-        trial_k = de_ser_int(I_left) >> 1
-        if trial_k < bls_curve_order:
-            return I
-        data = bytes_xor(data, I_left)
+        hashed_int = sha256(x.to_bytes(32, byteorder='big'))
+        x = int.from_bytes(hashed_int, byteorder='big')
+        if x < bls_curve_order and x != 0:
+            return x
 
 
-def derive_child_privkey(k_par: int, c_par: bytes, i: int) -> Tuple[int, bytes]:
-    data = b'\x00' + ser_256(k_par) + ser_32(i) if i >= 2**31 else ser_p(point(k_par)) + ser_32(i)
-    I = get_valid_I(chain_code=c_par, data=data)  # noqa: E741
-    I_left = I[:32]
-    I_right = I[32:]
-    k_i = (de_ser_int(I_left) >> 1 + k_par) % bls_curve_order
-    c_i = I_right
-    assert k_i != 0
-    return k_i, c_i
+def derive_child_privkey(k_par: int, i: int) -> int:
+    i_hash = int_to_int_hash(i)
+    k_hash = int_to_int_hash(k_par)
+    mod_sum = (i_hash + k_hash) % bls_curve_order
+    return mod_sum if i < 2**31 else int_to_int_hash(mod_sum)
 
 
-def derive_master_privkey(seed: bytes) -> Tuple[int, bytes]:
-    I = get_valid_I(chain_code=str.encode('Seed'), data=seed)  # noqa: E741
-    I_left = I[:32]
-    I_right = I[32:]
-    k_i = (de_ser_int(I_left) >> 1) % bls_curve_order
-    c_i = I_right
-    assert k_i != 0
-    return k_i, c_i
+def derive_master_privkey(seed: bytes) -> int:
+    assert len(seed) == 32
+    int_seed = int.from_bytes(seed, byteorder='big')
+    return int_to_int_hash(int_seed)
