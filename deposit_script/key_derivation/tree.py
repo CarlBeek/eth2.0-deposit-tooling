28719179f9697bfcb627d9674e469ad950bfa9b1
Carl Beekhuizen <carl.beek@gmail.com>
2019-09-12 23:07:16 +0200

    Many bug fixes

--- deposit_script/key_derivation/tree.py
+++ deposit_script/key_derivation/tree.py
@@ -6,14 +6,14 @@ from utils.bls import bls_curve_order
 from typing import List
 
 
-def flip_bits(input: int) -> bytes:
+def flip_bits(input: int) -> int:
     return input ^ (2**256 - 1)
 
 
 def seed_to_lamport_keys(seed: int, index: int) -> List[bytes]:
     combined_bytes = hkdf(master=seed.to_bytes(32, byteorder='big'),
                           salt=index.to_bytes(32, byteorder='big'), key_len=8160, hashmod=sha256)
-    return [combined_bytes[i: i + 32] for i in range(31)]
+    return [combined_bytes[i: i + 32] for i in range(255)]
 
 
 def parent_privkey_to_lamport_root(parent_key: int, index: int) -> bytes:
@@ -21,18 +21,19 @@ def parent_privkey_to_lamport_root(parent_key: int, index: int) -> bytes:
     lamport_1 = seed_to_lamport_keys(flip_bits(parent_key), index)
     lamport_privkeys = lamport_0 + lamport_1
     lamport_pubkeys = [sha256(sk) for sk in lamport_privkeys]
-    return sha256(lamport_pubkeys(lamport_pubkeys.join(b'')))
+    return sha256(b''.join(lamport_pubkeys))
 
 
 def hkdf_mod_r(ikm: bytes) -> int:
-    okm = hkdf(master=ikm, salt="BLS-SIG-KEYGEN-SALT-", key_len=48, hashmod=sha256)
+    okm = hkdf(ikm=ikm, salt=b'BLS-SIG-KEYGEN-SALT-', key_len=48)
     return int.from_bytes(okm, byteorder='big') % bls_curve_order
-
-
-def derive_master_privkey(seed: bytes) -> int:
-    return derive_child_privkey(int.from_bytes(seed, byteorder='big'), 0)
 
 
 def derive_child_privkey(parent_privkey: int, i: int) -> int:
     lamport_root = parent_privkey_to_lamport_root(parent_privkey, i)
     return hkdf_mod_r(lamport_root)
+
+
+def derive_master_privkey(seed: bytes) -> int:
+    seed = seed[:32]  # truncate seed to prevent integer casting overflow
+    return derive_child_privkey(int.from_bytes(seed, byteorder='big'), 0)
