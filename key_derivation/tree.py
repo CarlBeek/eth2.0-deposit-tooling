678ebb9701afec2ba14bf91abe9773c94365130f
Carl Beekhuizen <carl@ethereum.org>
2019-10-20 18:26:10 +0200

    Refactor to match specs

--- key_derivation/tree.py
+++ key_derivation/tree.py
@@ -10,18 +10,18 @@ def flip_bits(input: int) -> int:
     return input ^ (2**256 - 1)
 
 
-def IKM_to_lamport_SK(*, IKM: bytes, index: int) -> List[bytes]:
-    OKM = HKDF(salt=index.to_bytes(32, byteorder='big'),
-               IKM=IKM, L=8160)
+def IKM_to_lamport_SK(*, IKM: bytes, salt: bytes) -> List[bytes]:
+    OKM = HKDF(salt=salt, IKM=IKM, L=8160)
     lamport_SK = [OKM[i: i + 32] for i in range(0, 8160, 32)]
     return lamport_SK
 
 
 def parent_SK_to_lamport_PK(*, parent_SK: int, index: int) -> bytes:
+    salt = index.to_bytes(32, byteorder='big')
     IKM = parent_SK.to_bytes(32, byteorder='big')
-    lamport_0 = IKM_to_lamport_SK(IKM=IKM, index=index)
+    lamport_0 = IKM_to_lamport_SK(IKM=IKM, salt=salt)
     not_IKM = flip_bits(parent_SK).to_bytes(32, byteorder='big')
-    lamport_1 = IKM_to_lamport_SK(IKM=not_IKM, index=index)
+    lamport_1 = IKM_to_lamport_SK(IKM=not_IKM, salt=salt)
     lamport_SKs = lamport_0 + lamport_1
     lamport_PKs = [SHA256(sk) for sk in lamport_SKs]
     compressed_PK = SHA256(b''.join(lamport_PKs))
